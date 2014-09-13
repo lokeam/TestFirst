d6c3a07b69f2ac92d86735c6973737c7d8e3ed0e
Ahnming Loke <lokeam@users.noreply.github.com>
2014-09-13 17:35:55 -0400

    Update pigLatin.js

--- 05_pig_latin/pigLatin.js
+++ 05_pig_latin/pigLatin.js
@@ -1,50 +1,58 @@
 var translate = function(str){
-	var firstChar = str.charAt(0),
-      secondChar = str.charAt(1),
-      thirdChar = str.charAt(2),
-      translated = '';
+	var translated = '',
+      strArray = str.split(' ');
 
 	// function used for checking vowels
-	function isVowel(char){
+	function isVowel(char) {
 		if (char === 'a' || char === 'A' || char === 'e' || char === 'E' || char === 'i' || char === 'I' || char === 'o' || char === 'O' || char === 'u' || char === 'Y') {
 			return true;
 		}
 		return false;
 	};
 
-	// three consonants
-	if ( isVowel(firstChar) == false && isVowel(secondChar) == false && isVowel(thirdChar) == false) {
-    translated = str.slice(3) + str.slice(0, 3) + 'ay';
-    console.log(translated);
-	}
-  // counts 'qu' as a consonant even when it's preceded by a consonant 
-  else if ( isVowel(firstChar) == false && secondChar == 'q' && thirdChar == 'u') {
-    translated = str.slice(3) + str.slice(0, 3) + 'ay';
-    console.log(translated);
+  // function used for switching letters
+  function pLatinize(word) {
+    var firstChar = word.charAt(0),
+        secondChar = word.charAt(1),
+        thirdChar = word.charAt(2),
+        output = '';
+    
+    // vowel
+    if ( isVowel(firstChar) == true ) {
+      output = word + 'ay';
+	  }  
+	  // three consonants
+	  else if ( isVowel(firstChar) == false && isVowel(secondChar) == false && isVowel(thirdChar) == false) {
+      output = word.slice(3) + word.slice(0, 3) + 'ay';
+	  }
+    // counts 'qu' as a consonant even when it's preceded by a consonant 
+    else if ( isVowel(firstChar) == false && secondChar == 'q' && thirdChar == 'u') {
+      output = word.slice(3) + word.slice(0, 3) + 'ay';
+    }
+    // counts 'qu' as a single phoneme
+    else if ( firstChar == 'q' && secondChar == 'u') {
+      output = word.slice(2) + word.slice(0, 2) + 'ay';
+    }    
+	  // two consonants
+	  else if ( isVowel(firstChar) == false && isVowel(secondChar) == false ) {
+      output = word.slice(2) + word.slice(0, 2) + 'ay';
+	  }
+    // one consonant
+	  else {
+      output = word.slice(1) + word.slice(0, 1) + 'ay';
+	  }
+    return output;
   }
   
-	// two consonants
-	else if ( isVowel(firstChar) == false && isVowel(secondChar) == false ) {
-		console.log("ding");
-    translated = str.slice(2) + str.slice(0, 2) + 'ay';
-    console.log(translated);
-	}
-  
-  // one consonant
-	else if ( isVowel(firstChar) == false ) {
-		console.log("ding");
-    translated = str.slice(1) + str.slice(0, 1) + 'ay';
-    console.log(translated);
-	}
-  
-  // vowel
-	else if ( isVowel(firstChar) == true ) {
-		console.log("ding");
-    translated = str + 'ay';
-    console.log(translated);
-	}  
-  
+  // iterate through all words
+  for (var i = 0; i < strArray.length; i++) {
+    strArray[i] = pLatinize(strArray[i]);  
+    
+  }
+
+  return strArray.join(" ");
+    
 };
 
-translate('apple');
+translate('one two three square five queen');
 
