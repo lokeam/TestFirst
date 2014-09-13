85711e11273fcc7f61ac9fe81c56f1098e999100
Ahnming Loke <lokeam@users.noreply.github.com>
2014-09-13 16:12:33 -0400

    Create pigLatin.js

---
+++ 05_pig_latin/pigLatin.js
@@ -0,0 +1,36 @@
+var translate = function(str){
+	var firstChar = str.charAt(0),
+      secondChar = str.charAt(1),
+      thirdChar = str.charAt(2),
+      translated = '';
+
+	// function used for checking vowels
+	function isVowel(char){
+		if (char === 'a' || char === 'A' || char === 'e' || char === 'E' || char === 'i' || char === 'I' || char === 'o' || char === 'O' || char === 'u' || char === 'Y') {
+			return true;
+		}
+		return false;
+	};
+
+	// three consonants
+	if ( isVowel(firstChar) == false && isVowel(secondChar) == false && isVowel(thirdChar) == false) {
+    translated = str.slice(3) + str.slice(0, 3) + 'ay';
+    console.log(translated);
+	}
+  // counts 'qu' as a consonant even when it's preceded by a consonant 
+  else if ( isVowel(firstChar) == false && secondChar == 'q' && thirChar == 'u') {
+    translated = str.slice(3) + str.slice(0, 3) + 'ay';
+    console.log(translated);
+  }
+  
+	// two consonants
+	else if ( isVowel(firstChar) == false && isVowel(secondChar) == false ) {
+		console.log("ding");
+    translated = str.slice(2) + str.slice(0, 2) + 'ay';
+    console.log(translated);
+	}
+
+};
+
+translate('two');
+
